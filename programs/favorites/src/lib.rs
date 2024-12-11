a9f6a5236ec4343e8c6d9dd6942de86c42f1dd19
Muhammed Ameen <ameens.in19@gmail.com>
2024-12-11 12:49:29 +0530

    first commit

---
+++ programs/favorites/src/lib.rs
@@ -0,0 +1,43 @@
+use anchor_lang::prelude::*;
+
+declare_id!("");
+
+pub const ANCHOR_DISCRIMINATOR_SIZE: usize = 8;
+
+#[program]
+pub mod favorites {
+    use super::*;
+
+    pub fn set_favorites() -> Return<()> {
+        msg!("Greetings from {}", context.programIe);
+        //fill
+    }
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct Favorites {
+    pub number: u64,
+
+    #[max_len(50)]
+    pub color: String,
+
+    #[max_len(50)]
+    pub hobbies: Vec<String>,
+}
+
+pub struct SetFavorites<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    #[account(
+        init_if_needed,
+        payer = user,
+        space = ANCHOR_DISCRIMINATOR_SIZE + Favorites::INIT_SPACE
+        seeds = [b"favorites", user.key().as_ref()],
+        bump 
+    )]
+    pub favorites: Account<'info, Favorites>,
+
+    pub system_program: Program<'info,System>
+}
