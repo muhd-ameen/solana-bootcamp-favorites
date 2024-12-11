2e9ebeada4cff47b968f01336b7210ebf8f5ec26
Muhammed Ameen <ameens.in19@gmail.com>
2024-12-11 13:02:18 +0530

    updated

--- programs/favorites/src/lib.rs
+++ programs/favorites/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("");
+declare_id!("FgbSdCFiVN5PeA4dY3iQUYjAw9JpYPQrfAyvigFTJ6HN");
 
 pub const ANCHOR_DISCRIMINATOR_SIZE: usize = 8;
 
@@ -8,9 +8,25 @@ pub const ANCHOR_DISCRIMINATOR_SIZE: usize = 8;
 pub mod favorites {
     use super::*;
 
-    pub fn set_favorites() -> Return<()> {
-        msg!("Greetings from {}", context.programIe);
-        //fill
+    // Our instruction handler! It sets the user's favorite number and color
+    pub fn set_favorites(
+        context: Context<SetFavorites>,
+        number: u64,
+        color: String,
+        hobbies: Vec<String>,
+    ) -> Result<()> {
+        let user_public_key = context.accounts.user.key();
+        msg!("Greetings from {}", context.program_id);
+        msg!("User {user_public_key}'s favorite number is {number}, favorite color is: {color}",);
+
+        msg!("User's hobbies are: {:?}", hobbies);
+
+        context.accounts.favorites.set_inner(Favorites {
+            number,
+            color,
+            hobbies,
+        });
+        Ok(())
     }
 }
 
@@ -22,22 +38,22 @@ pub struct Favorites {
     #[max_len(50)]
     pub color: String,
 
-    #[max_len(50)]
+    #[max_len(5, 50)]
     pub hobbies: Vec<String>,
 }
-
+// When people call the set_favorites instruction, they will need to provide the accounts that will be modifed. This keeps Solana fast!
+#[derive(Accounts)]
 pub struct SetFavorites<'info> {
     #[account(mut)]
     pub user: Signer<'info>,
 
     #[account(
-        init_if_needed,
-        payer = user,
-        space = ANCHOR_DISCRIMINATOR_SIZE + Favorites::INIT_SPACE
-        seeds = [b"favorites", user.key().as_ref()],
-        bump 
-    )]
+        init_if_needed, 
+        payer = user, 
+        space = ANCHOR_DISCRIMINATOR_SIZE + Favorites::INIT_SPACE, 
+        seeds=[b"favorites", user.key().as_ref()],
+    bump)]
     pub favorites: Account<'info, Favorites>,
 
-    pub system_program: Program<'info,System>
+    pub system_program: Program<'info, System>,
 }
